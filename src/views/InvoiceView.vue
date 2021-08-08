0885541a88bc905e5bfd3a5c93527244e9d1ba84
alex-codeur <apachecordovax@gmail.com>
2021-08-08 17:14:56 +0200

    firebase: suppression d'un invoice

--- src/views/InvoiceView.vue
+++ src/views/InvoiceView.vue
@@ -109,7 +109,7 @@
 </template>
 
 <script>
-import { mapMutations, mapState } from "vuex";
+import { mapActions, mapMutations, mapState } from "vuex";
 export default {
   name: "invoiceView",
   data() {
@@ -127,6 +127,8 @@ export default {
       "TOGGLE_INVOICE",
     ]),
 
+    ...mapActions(["DELETE_INVOICE"]),
+
     getCurrentInvoice() {
       this.SET_CURRENT_INVOICE(this.$route.params.invoiceId);
       this.currentInvoice = this.currentInvoiceArray[0];
@@ -135,6 +137,11 @@ export default {
     toggleEditInvoice() {
       this.TOGGLE_EDIT_INVOICE();
       this.TOGGLE_INVOICE();
+    },
+
+    async deleteInvoice(docId) {
+      await this.DELETE_INVOICE(docId);
+      this.$router.push({ name: "Home" });
     },
   },
   computed: {
